69a296ea04a5e2270cecf9318d45cc7115e2731c
Heru Joko P. Utomo <heru.utomo@grabtaxi.com>
2023-11-02 16:08:09 +0800

    other pages

--- src/pages/dashboard/profile.tsx
+++ src/pages/dashboard/profile.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import DashboardLayout from './layout';
+import css from './profile.module.css';
+import { Button, Card, Divider } from 'antd';
+import { PoweroffOutlined } from '@ant-design/icons';
 
 const DashboardProfilePage = () => {
     return (
         <DashboardLayout>
-            <div>profile content</div>
+            <div className={css.container}>
+                <Card>
+                    <div>
+                        <span>Nama Anda</span><br />
+                        <small>email@mail.me</small>
+                    </div>
+                    <Divider />
+                    <Button danger type='primary' icon={<PoweroffOutlined />}>keluar</Button>
+                </Card>
+            </div>
         </DashboardLayout>
     );
 };
--- src/pages/dashboard/sales.tsx
+++ src/pages/dashboard/sales.tsx
@@ -1,12 +1,42 @@
 import React from 'react';
 import DashboardLayout from './layout';
 import SalesToolbar from '@feature/sales-toolbar';
+import { MonthlySales } from 'types/sales';
+import SalesItemInfo from '@common/component/sales-item-info';
 
 const DashboardSalesPage = () => {
+    const onChange = () => {};
+
+    const mock: MonthlySales[] = [
+        {
+            amount: 8000000,
+            month: '2023-10-26T01:45:51.249Z',
+            region: {
+                id: 'shadgsjh',
+                name: 'Balikpapan',
+            },
+        },
+    ];
+
     return (
         <DashboardLayout>
-            <div>sales content</div>
-            <SalesToolbar />
+            <SalesToolbar onChange={onChange} />
+            <br />
+            <div>
+                <SalesItemInfo sales={mock[0]} />
+                <SalesItemInfo sales={mock[0]} />
+                <SalesItemInfo sales={mock[0]} />
+                <SalesItemInfo sales={mock[0]} />
+                <SalesItemInfo sales={mock[0]} />
+                <SalesItemInfo sales={mock[0]} />
+                <SalesItemInfo sales={mock[0]} />
+                <SalesItemInfo sales={mock[0]} />
+                <SalesItemInfo sales={mock[0]} />
+                <SalesItemInfo sales={mock[0]} />
+                <SalesItemInfo sales={mock[0]} />
+                <SalesItemInfo sales={mock[0]} />
+                <SalesItemInfo sales={mock[0]} />
+            </div>
         </DashboardLayout>
     );
 };
